90bca9f5b6316b82a89781a3761d1aeb9ca1ab85
drwiner <drwiner131@gmail.com>
2018-04-05 11:56:51 -0600

    Ready for class

--- MachineLearning/Test.cs
+++ MachineLearning/Test.cs
@@ -1,4 +1,5 @@
 ﻿using MachineLearning.DecisionTree;
+using MachineLearning.Enums;
 using MachineLearning.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,10 +11,40 @@ namespace MachineLearning
 {
     public class Test
     {
-        static void Main(string[] args)
+
+        // A random observation generator
+        public static List<Observation> ObservationGenerator(int numObs, List<IFeature> Feats, Dictionary<IFeature, List<IValue>> featValueDict, List<Label> labels)
+        {
+            // Number of values for each attribute/feature
+            var valueCountDict = featValueDict.ToDictionary(fv => fv.Key, fv => fv.Value.Count);
+
+            // Blank liset of observations
+            var obsList = new List<Observation>();
+
+            // System must be specified for inclusion with Unity
+            var rnd = new System.Random();
+
+            // Generating numbObs new observations
+            for (int i =0; i< numObs; i++)
+            {
+                // Select n random numbers for n = Feats.Count;
+                var obsDict = Feats.ToDictionary(feat => feat, feat => featValueDict[feat][rnd.Next(valueCountDict[feat])] as IValue);
+
+                // Pick random label
+                var label = labels[rnd.Next(0, labels.Count)];
+
+                // Create and add new observation
+                obsList.Add(new Observation(obsDict as Dictionary<IFeature, IValue>, label));
+            }
+
+            // return observation List
+            return obsList;
+        }
+
+        public static void HandCodedDT()
         {
             var featValueDict = new Dictionary<IFeature, List<IValue>>();
-            var feats = new List<Feature>() { new Feature(), new Feature(), new Feature(), new Feature() };
+            var feats = new List<IFeature>() { new Feature() as IFeature, new Feature() as IFeature, new Feature() as IFeature, new Feature() as IFeature };
             featValueDict[feats[0]] = new List<IValue>() { new FeatValue(0), new FeatValue(1) };
             featValueDict[feats[1]] = new List<IValue>() { new FeatValue(3), new FeatValue(18), new FeatValue(20) };
             featValueDict[feats[2]] = new List<IValue>() { new FeatValue(0), new FeatValue(1) };
@@ -74,6 +105,46 @@ namespace MachineLearning
             var prediction = DT.UseTree(testObs);
             Console.WriteLine(prediction);
             Console.ReadLine();
+        }
+
+        public static void TestDecisionTree()
+        {
+            var featValueDict = new Dictionary<IFeature, List<IValue>>();
+            var feats = new List<IFeature>() { new Feature() as IFeature, new Feature() as IFeature, new Feature() as IFeature, new Feature() as IFeature };
+            featValueDict[feats[0]] = new List<IValue>() { new FeatValue(0), new FeatValue(1) };
+            featValueDict[feats[1]] = new List<IValue>() { new FeatValue(3), new FeatValue(18), new FeatValue(20) };
+            featValueDict[feats[2]] = new List<IValue>() { new FeatValue(0), new FeatValue(1) };
+            featValueDict[feats[3]] = new List<IValue>() { new FeatValue(0), new FeatValue(1), new FeatValue(2), new FeatValue(3) };
+
+            var labelList = new List<Enums.Label>() { Enums.Label.up, Enums.Label.down, Enums.Label.left, Enums.Label.right };
+            var obsList = ObservationGenerator(200, feats, featValueDict, labelList);
+
+
+            var root = ID3.Learn(obsList, featValueDict, labelList, 3);
+            var DT = new DTree(root);
+
+            var testObsList = ObservationGenerator(200, feats, featValueDict, labelList);
+
+            // how many correct?
+            var sumCorrect = 0;
+
+            // Precision: given guess of this label, how many correct?
+            var precisionDict = new Dictionary<Label, int>();
+
+            // Recall: given actual label, how many correct?
+            var recallDict = new Dictionary<Label, int>();
+
+            foreach (var testobs in testObsList)
+            {
+                // Do total, precision, and recall here.
+            }
+          
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            TestDecisionTree();
         }
     }
 }
